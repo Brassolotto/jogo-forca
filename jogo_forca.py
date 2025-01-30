34abbed7614bbac221441fefc26815e0f76d4fc1
Ricardo Brassolotto <23336979+Brassolotto@users.noreply.github.com>
2025-01-30 15:29:44 -0300

    Implementada a lógica inicial

--- jogo_forca.py
+++ jogo_forca.py
@@ -1,1 +1,78 @@
-#bora pensar no jogo
+import random
+
+def escolher_palavra():
+    palavras = ['PYTHON', 'PROGRAMACAO', 'COMPUTADOR', 'DESENVOLVIMENTO', 'TECNOLOGIA']
+
+    return random.choice(palavras)
+
+def mostrar_forca(erros):
+    estagios = [
+        """
+           +---+
+           |   |
+               |
+               |
+               |
+               |
+        =========
+        """,
+        """
+           +---+
+           |   |
+           O   |
+               |
+               |
+               |
+        =========
+        """,
+        """
+           +---+
+           |   |
+           O   |
+           |   |
+               |
+               |
+        =========
+        """,
+        """
+           +---+
+           |   |
+           O   |
+          /|   |
+               |
+               |
+        =========
+        """,
+        """
+           +---+
+           |   |
+           O   |
+          /|\\  |
+               |
+               |
+        =========
+        """,
+        """
+           +---+
+           |   |
+           O   |
+          /|\\  |
+          /    |
+               |
+        =========
+        """,
+        """
+           +---+
+           |   |
+           O   |
+          /|\\  |
+          / \\  |
+               |
+        =========
+        """
+    ]
+
+    return estagios[erros]
+
+
+    
